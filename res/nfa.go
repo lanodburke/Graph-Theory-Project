3bc1d34f43b996a9add2c47b4b5b90c489b62257
Donal Burke <dbrke98@gmail.com>
2018-04-08 22:49:54 +0100

    Cleaned up code and addded comments

--- res/nfa.go
+++ res/nfa.go
@@ -16,63 +16,93 @@ type Nfa struct {
 }
 
 // Poregtonfa function returns *nfa
+// Postfix regular expression to non-deterministic finite automaton
 func Poregtonfa(pofix string) *Nfa {
 	// array of pointers to nfa structs
 	nfastack := []*Nfa{}
 
+	// Looping over postfix expression character by character
 	for _, r := range pofix {
 		switch r {
+			// match any element except newline
 			case '.':
+				// Pop last element off the stack
 				frag2 := nfastack[len(nfastack)-1]
+				// Remove last element off the stack
 				nfastack = nfastack[:len(nfastack)-1]
+				// Pop last element off the stack
 				frag1 := nfastack[len(nfastack)-1]
+				// Then remove the element from the stack
 				nfastack = nfastack[:len(nfastack)-1]
-
+				
+				// Join accept state of first fragment to accept state of the second fragment
 				frag1.accept.edge1 = frag2.initial
-
+				
+				// Then append the new Nfa fragment to the stack
 				nfastack = append(nfastack, &Nfa{initial: frag1.initial, accept: frag2.accept})
 
+			// seperate any alternative characters
 			case '|':
 				frag2 := nfastack[len(nfastack)-1]
 				nfastack = nfastack[:len(nfastack)-1]
 				frag1 := nfastack[len(nfastack)-1]
 				nfastack = nfastack[:len(nfastack)-1]
 
+				// Points two edges to inital states of the fragments
 				initial := State{edge1: frag1.initial, edge2: frag2.initial}
 				accept := State{}
 
+				// Set frag1 and frag2's edge to the accept state
 				frag1.accept.edge1 = &accept
 				frag2.accept.edge1 = &accept
 
+				// Push new nfa onto the stack
 				nfastack = append(nfastack, &Nfa{initial: &initial, accept: &accept})
 			
+			// zero or more characters
 			case '*':
+				// Pop one fragment off the stack
 				frag := nfastack[len(nfastack)-1]
+				// Remove fragment from the stack
 				nfastack = nfastack[:len(nfastack)-1]
 
+				// New accept state
 				accept := State{}
+				// Set inital state to frag1's inital state and the accept stage
 				initial := State{edge1: frag.initial, edge2: &accept}
 				
 				frag.accept.edge1 = frag.initial
 				frag.accept.edge2 = &accept
 
+				// push nfa onto the stack
 				nfastack = append(nfastack, &Nfa{initial: &initial, accept: &accept})
-				case '+':
-					frag := nfastack[len(nfastack)-1]
+			
+			// match one or more than one element 
+			case '+':
+				// Pop fragment off the stack
+				frag := nfastack[len(nfastack)-1]
+				// Remove elemt from the stack
+				nfastack = nfastack[:len(nfastack)-1]
+				// Set accept state
+				accept := state{}
+				initial := state{edge1: frag.initial, edge2: &accept}
 
-					accept := state{}
-					initial := state{edge1: frag.initial, edge2: &accept}
+				// set fragments accept state to inital state
+				frag.accept.edge2 = &initial
 
-					frag.accept.edge1 = &initial
+				// push new nfa onto the stack
+				nfastack = append(nfastack, &nfa{initial: frag.initial, accept: &accept})
+			
+			// match one or none elements
+			case '?':
+				// Pop fragment off of the stack
+				frag := nfastack[len(nfastack)-1]
+				// Then remove the element from the stack
+				nfastack = nfastack[:len(nfastack)-1]
 
-					nfastack = append(nfastack, &nfa{initial: frag.initial, accept: &accept})
-				case '?':
-					frag := nfastack[len(nfastack)-1]
-					nfastack = nfastack[:len(nfastack)-1]
+				initial := state{edge1: frag.initial, edge2: frag.accept}
 
-					initial := state{edge1: frag.initial, edge2: frag.accept}
-
-					nfastack = append(nfastack, &nfa{initial: &initial, accept: frag.accept})
+				nfastack = append(nfastack, &nfa{initial: &initial, accept: frag.accept})
 						
 			default:
 				accept := State{}
@@ -108,20 +138,26 @@ func Pomatch(po string, s string) bool {
 	ismatch := false
 	ponfa := Poregtonfa(po)
 
+	// current states
 	current := []*State{}
+	// next states
 	next := []*State{}
 
 	current = addState(current[:], ponfa.initial, ponfa.accept)
 
+	// for every character in s loop over every character in c and compare them one by one
 	for _, r := range s {
 		for _, c := range current {
+			// if they are equal
 			if c.symbol == r {
+				// if current state is equal to rune character
 				next = addState(next[:], c.edge1, ponfa.accept)
 			}
 		}
 		current, next = next, []*State{}
 	}
 
+	// if they are in the accept state then the regular expression matches the nfa
 	for _, c := range current {
 		if c == ponfa.accept {
 			ismatch = true
--- res/shunt.go
+++ res/shunt.go
@@ -2,20 +2,30 @@ package res
 
 // IntoPost function
 func IntoPost(infix string) string {
+	// Map of speacial characters mapped to numbers, higher the number higher the presidence of the operator
 	specials := map[rune]int{'*':10, '.': 9, '|': 8, '?': 7, '+': 6}
 
-	pofix, stack := []rune{}, []rune{}
+	// Array of rune's, rune's are any unicode character
+	// Postfix will be the expression re-written after the loop has finished in post fix notation
+	// Stack is where operators are stored in the infix expression
+	pofix, stack := []rune{}, []rune{} 
 
+	// Loop over infix expression character by character
 	for _, r := range infix {
 		switch {
 		case r == '(':
+			// append onto the end of the stack
 			stack = append(stack, r)
 		case r == ')':
+			// pop characters off the stack until we see an open bracket and append them onto postfix expression
 			for stack[len(stack)-1] != '(' {
 				pofix, stack = append(pofix, stack[len(stack)-1]), stack[:len(stack)-1]
 			}
 			stack = stack[:len(stack)-1]
 		case specials[r] > 0:
+			// if there are elements on the stack and the presidence of the current character 
+			// is less than the presidence of the character on the top of the stack, pop elements 
+			// off the stack and append them to the postfix expression 
 			for len(stack) > 0 && specials[r] <= specials[stack[len(stack)-1]] {
 				pofix, stack = append(pofix, stack[len(stack)-1]), stack[:len(stack)-1]
 			}
